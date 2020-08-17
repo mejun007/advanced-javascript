f0a2e6d65960333b5d321b862d8f55faeab07cac
mejun007 <67540246+mejun007@users.noreply.github.com>
2020-08-18 04:23:16 +0600

    bujhlm halka patla

---
+++ scope.js
@@ -0,0 +1,9 @@
+function sum(first,second){
+    let result= first+second;
+    return result;
+}
+
+//console.log(reult) kora jabe na ,scope nai,function er vitore korte hbe
+
+const output =sum(2,4);
+console.log(output);
