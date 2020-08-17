d79c13a92ab805b03e2a3f8999d00be571dfa41c
mejun007 <67540246+mejun007@users.noreply.github.com>
2020-08-18 04:58:07 +0600

    bujhlm kichu

---
+++ array-split.js
@@ -0,0 +1,7 @@
+const nums=[1,5,2,8,4,7,3,5,8,4];
+//const part = nums.slice(2,5);
+const removed = nums.splice(2,5,19,77,66,44);
+//console.log(removed);
+//console.log(nums);
+const together= nums.join(",");
+console.log(together);
