2e3ce107fc0576ecd3dcd1eacf94a468c4815082
mejun007 <67540246+mejun007@users.noreply.github.com>
2020-08-18 02:54:21 +0600

    final

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,8 +1,8 @@
-//let pakhi;
- //function add (num1,num2){
- //    console.log(num2);
- //}
-//const premik = {name:"smart dude", phone:2674785}
-//console.log(premik.gf);
+let pakhi;
+ function add (num1,num2){
+    console.log(num2);
+ }
+const premik = {name:"smart dude", phone:2674785}
+console.log(premik.gf);
  let fun = undefined;
  console.log(fun);
