3265e4d8f6872560d26949d70f321aa8a7f7bf6f
mejun007 <67540246+mejun007@users.noreply.github.com>
2020-08-18 03:49:03 +0600

    map

---
+++ map-filter.js
@@ -0,0 +1,20 @@
+const number =[2,6,3,8,4,9];
+//const output=[];
+
+//for(let i = 0;i < number.length ; i++){
+  //  const element=number[i];
+   // const result= element * element;
+    //output.push(result);
+//}
+//const square = element => element*element;
+//const square = x=> x*x;
+//const result=number.map(function(element){
+  //  return element*element;
+//})
+// const result = number.map(x=>x*x);
+
+//console.log(result);
+//const bigger= number.filter(x=> x>5);
+//console.log(bigger);
+const isThere = number.find(x=> x>5);
+console.log(isThere);
